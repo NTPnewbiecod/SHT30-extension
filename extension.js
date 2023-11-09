44f478cdabe01c1e253751e7987a66db3f6bdd0c
Max <sonthaya.nt@hotmail.com>
2023-11-09 11:23:01 +0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -3,7 +3,7 @@
     description: "Humidity and Temperature Sensor",
     author: "IOXhop.com",
     category: "Sensors",
-    version: "1.0.0",
+    version: "1.0.1",
     icon: "/static/icon.png", // Category icon
     color: "#8b507c", // Category color (recommend some blocks color)
     blocks: [ // Blocks in Category
