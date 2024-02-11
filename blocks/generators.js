929b764fc9e25a35454088cbc39d4f6155b9354b
Max <7087907+maxpromer@users.noreply.github.com>
2024-02-11 17:50:02 +0700

    Add Arduino support

--- blocks/generators.js
+++ blocks/generators.js
@@ -7,3 +7,37 @@ Blockly.Python['sht30_read'] = function (block) {
   var code = `SHT30.read(${dropdown_addr})[${dropdown_type}]`;
   return [code, Blockly.Python.ORDER_NONE];
 };
+
+Blockly.JavaScript['sht30_read'] = function (block) {
+  Blockly.JavaScript.definitions_['include']['ArtronShop_SHT3x.h'] = '#include <ArtronShop_SHT3x.h>';
+
+  var dropdown_addr = block.getFieldValue('addr');
+  var dropdown_type = block.getFieldValue('type');
+
+  var functionName = Blockly.Python.provideFunction_(
+    'SHT30_read',
+    [
+      'float ' + Blockly.Python.FUNCTION_NAME_PLACEHOLDER_ + '(uint8_t addr, uint8_t type) {',
+      '  static ArtronShop_SHT3x * sht3x = NULL;',
+      '  static last_addr = 0;',
+      '  if ((!sht3x) || (last_addr != addr)) {',
+      '    if (sht3x) {',
+      '      delete sht3x;',
+      '      sht3x = NULL;',
+      '    }',
+      '    sht3x = new ArtronShop_SHT3x(addr, &Wire);',
+      '    Wire.begin();',
+      '    sht3x->begin();',
+      '    last_addr = addr;',
+      '  }',
+      '  sht3x->measure();',
+      '  return type == 0 ? sht3x->temperature() : sht3x->humidity();',
+      '}'
+    ]);
+
+  var value_pin = Blockly.Python.valueToCode(block, 'pin', Blockly.Python.ORDER_ATOMIC);
+  var code = `${functionName}(${value_pin})`;
+
+  var code = `${functionName}(${dropdown_addr}, ${dropdown_type})`;
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
--- extension.js
+++ extension.js
@@ -1,12 +1,16 @@
 ({
     name: "SHT30", // Category Name
-    description: "Humidity and Temperature Sensor",
-    author: "IOXhop.com",
+    description: "Temperature and Humidity Sensor",
+    author: "ArtronShop",
     category: "Sensors",
-    version: "1.0.1",
+    version: "1.1.0",
     icon: "/static/icon.png", // Category icon
     color: "#8b507c", // Category color (recommend some blocks color)
     blocks: [ // Blocks in Category
-        "sht30_read"
+        "sht30_read",
+    ], 
+    supportArduinoPlatform: true,
+    depends: [ // Arduino library
+        "ArtronShop_SHT3x@1.0.0"
     ]
 });
